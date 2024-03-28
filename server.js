0b9ab3bf1cd75f70c1fab7b12d6330310a5569e5
im2013 <128984092+amber-wolf0@users.noreply.github.com>
2024-03-28 23:43:28 +0000

    heroku enabled

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000; // Choose any available port number
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to handle incoming GET requests
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'css', 'index.css'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
